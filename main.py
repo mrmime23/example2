668ff8df9a97c50a9647af2452a6b263c60b4555
mrmime23 <143339797+mrmime23@users.noreply.github.com>
2023-08-30 19:03:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,162 +1,146 @@
-from kivy.app import A
-from kivy.uix.button import B1
-from kivy.uix.label import B2
-from kivy.uix.textinput import C
-from kivy.uix.boxlayout import D
-from kivy.uix.screenmanager import E, F
-from kivy.uix.scrollview import G
-from instructions import H, I, J
-from ruffier import K
-from kivy.uix.popup import L
-from kivy.core.window import M
-from coloredLayout import N
-from coloredLayout1 import O
+from kivy.app import A as ApP
+from kivy.uix.button import B1 as ButTon
+from kivy.uix.label import B2 as LabeL
+from kivy.uix.textinput import C as TextInPUT
+from kivy.uix.boxlayout import D as BoXLayOUT
+from kivy.uix.screenmanager import E as SCrEenMG, F as ScreeN
+from kivy.uix.scrollview import G as SCrollVIE
+from instructions import H as InSt1, I as InSt2, J as InSt3
+from ruffier import K as ruffIER
+from kivy.uix.popup import L as POPuP
+from kivy.core.window import M as WinDO
+from coloredLayout import N as CLayout
+from coloredLayout1 import O as CLayout1
 
-X = (.53, .53, .53, 1)
-M.Y = X
-Z = 7
-Q = ""
-R = 0
-S = 0
-T = 0
+BgCOL = (.53, .53, .53, 1)
+WinDO.Y = BgCOL
+AGE = 7
+Name = ""
+VarR = 0
+VarS = 0
+VarT = 0
 
-def U(V):
+def ToInt(V):
     try:
         return int(V)
     except:
         return False
 
-class W(F):
-    def __init__(self, **aa):
-        super().__init__(**aa)
-        bb = B2(text=H)
-        cc = D(orientation='vertical', padding=8, spacing=8)
-        dd = '[i][color=#003300]'+'Введите имя:'+'[/color][/i]'
-        ee = B2(text=dd, halign='right', markup=True)
-        self.ff = C(multiline=False)
-        gg = B2(text='Введите возраст:', halign='right')
-        self.hh = C(text='7', multiline=False)
-        self.ii = B1(text='Начать', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
-        jj = (1, 1, 0, .5)
-        self.ii.background_color = jj
-        self.ii.on_press = self.kk
-        ll = D(size_hint=(0.8, None), height='30sp')
-        mm = D(size_hint=(0.8, None), height='30sp')
-        ll.add_widget(ee)
-        ll.add_widget(self.ff)
-        mm.add_widget(gg)
-        mm.add_widget(self.hh)
-        cc.add_widget(bb)
-        cc.add_widget(ll)
-        cc.add_widget(mm)
-        cc.add_widget(self.ii)
-        self.add_widget(cc)
+class Scn1(ScreeN):
+    def __init__(self, **args_1):
+        super().__init__(**args_1)
+        labe1 = LabeL(text=InSt1)
+        boxL1 = BoXLayOUT(orientation='vertical', padding=8, spacing=8)
+        txt1 = '[i][color=#003300]'+'Введите имя:'+'[/color][/i]'
+        labe2 = LabeL(text=txt1, halign='right', markup=True)
+        self.TxtIn1 = TextInPUT(multiline=False)
+        labe3 = LabeL(text='Введите возраст:', halign='right')
+        self.TxtIn2 = TextInPUT(text='7', multiline=False)
+        self.But1 = ButTon(text='Начать', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        col1 = (1, 1, 0, .5)
+        self.But1.background_color = col1
+        self.But1.on_press = self.Func1
+        boxL2 = BoXLayOUT(size_hint=(0.8, None), height='30sp')
+        boxL3 = BoXLayOUT(size_hint=(0.8, None), height='30sp')
+        boxL2.add_widget(labe2)
+        boxL2.add_widget(self.TxtIn1)
+        boxL3.add_widget(labe3)
+        boxL3.add_widget(self.TxtIn2)
+        boxL1.add_widget(labe1)
+        boxL1.add_widget(boxL2)
+        boxL1.add_widget(boxL3)
+        boxL1.add_widget(self.But1)
+        self.add_widget(boxL1)
 
-    def kk(self):
-        global Z, Q
-        Q = self.ff.text
-        Z = self.hh.text
-        Z = U(Z)
-        if Z == False or Z < 7:
-            Z = 7
-            self.hh.text = str(Z)
-            nn = L(title='Error', content=B2(text='Возраст должен быть больше 7'), size_hint=(None, None), size=(300, 300), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-            nn.open()
+    def Func1(self):
+        global AGE, Name
+        Name = self.TxtIn1.text
+        AGE = self.TxtIn2.text
+        AGE = ToInt(AGE)
+        if AGE == False or AGE < 7:
+            AGE = 7
+            self.TxtIn2.text = str(AGE)
+            pop_1 = POPuP(title='Error', content=LabeL(text='Возраст должен быть больше 7'), size_hint=(None, None), size=(300, 300), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+            pop_1.open()
         else:
-            self.manager.current='oo'
+            self.manager.current='Scn2'
 
-class PP(F):
-    def __init__(self, **qq):
-        super().__init__(**qq)
-        rr = B2(text=I)
-        ss = D(orientation='vertical', padding=8, spacing=8)
-        tt = B2(text='Введите результат:', halign='right')
-        self.uu = C(multiline=False)
-        self.vv = B1(text='Продолжить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
-        self.vv.on_press = self.ww
-        self.xx = B1(text="Назад", size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
-        self.xx.on_press = self.yy
-        zz = N(lcolor=(0, 0.5, 0.01, 1))
-        zz.add_widget(tt)
-        zz.add_widget(self.uu)
-        aa1 = N(lcolor=(0.5, 0.5, 0.5, 1))
-        aa1.add_widget(self.xx)
-        aa1.add_widget(self.vv)
-        ss.add_widget(rr)
-        ss.add_widget(zz)
-        ss.add_widget(aa1)
-        self.add_widget(ss)
+class Scn2(ScreeN):
+    def __init__(self, **args_2):
+        super().__init__(**args_2)
+        labe4 = LabeL(text=InSt2)
+        boxL4 = BoXLayOUT(orientation='vertical', padding=8, spacing=8)
+        labe5 = LabeL(text='Введите результат:', halign='right')
+        self.TxtIn3 = TextInPUT(multiline=False)
+        self.But2 = ButTon(text='Продолжить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.But2.on_press = self.Func2
+        self.But3 = ButTon(text="Назад", size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.But3.on_press = self.Func3
+        colLay1 = CLayout(lcolor=(0, 0.5, 0.01, 1))
+        colLay1.add_widget(labe5)
+        colLay1.add_widget(self.TxtIn3)
+        colLay2 = CLayout(lcolor=(0.5, 0.5, 0.5, 1))
+        colLay2.add_widget(self.But3)
+        colLay2.add_widget(self.But2)
+        boxL4.add_widget(labe4)
+        boxL4.add_widget(colLay1)
+        boxL4.add_widget(colLay2)
+        self.add_widget(boxL4)
 
-    def ww(self):
-        global R
-        R = self.uu.text
-        R = U(R)
-        if R == False or R <= 0:
-            R = 0
-            self.uu.text = str(R)
+    def Func2(self):
+        global VarR
+        VarR = self.TxtIn3.text
+        VarR = ToInt(VarR)
+        if VarR == False or VarR <= 0:
+            VarR = 0
+            self.TxtIn3.text = str(VarR)
         else:
-            self.manager.current='bb1'
+            self.manager.current='Scn3'
 
-    def yy(self):
+    def Func3(self):
         self.manager.current = self.manager.previous()
 
-class CC(F):
-    def __init__(self, **dd1):
-        super().__init__(**dd1)
-        ee1 = B2(text=J)
-        ff1 = D(orientation='vertical', padding=8, spacing=8)
-        gg1 = B2(text='Введите результат:', halign='right')
-        self.hh1 = C(multiline=False)
-        self.ii1 = C(multiline=False)
-        self.jj1 = B1(text='Продолжить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
-        self.jj1.on_press = self.kk1
-        ll1 = O(lcolor=(0.44, 0.44, 0.44, 1))
-        ll1.add_widget(gg1)
-        ll1.add_widget(self.hh1)
-        ll1.add_widget(self.ii1)
-        ff1.add_widget(ee1)
-        ff1.add_widget(ll1)
-        ff1.add_widget(self.jj1)
-        self.add_widget(ff1)
+class Scn3(ScreeN):
+    def __init__(self, **args_3):
+        super().__init__(**args_3)
+        labe6 = LabeL(text=InSt3)
+        boxL5 = BoXLayOUT(orientation='vertical', padding=8, spacing=8)
+        labe7 = LabeL(text='Введите результат:', halign='right')
+        self.TxtIn4 = TextInPUT(multiline=False)
+        self.TxtIn5 = TextInPUT(multiline=False)
+        self.But4 = ButTon(text='Продолжить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.But4.on_press = self.Func4
+        colLay3 = CLayout1(lcolor=(0.44, 0.44, 0.44, 1))
+        colLay3.add_widget(labe7)
+        colLay3.add_widget(self.TxtIn4)
+        colLay3.add_widget(self.TxtIn5)
+        boxL5.add_widget(labe6)
+        boxL5.add_widget(colLay3)
+        boxL5.add_widget(self.But4)
+        self.add_widget(boxL5)
 
-    def kk1(self):
-        global S, T
-        S = self.hh1.text
-        S = U(R)
-        T = self.ii1.text
-        T = U(T)
-        if S == False or S <= 0:
-            S = 0
-            self.hh1.text = str(S)
-        elif T == False or T <= 0:
-            T = 0
-            self.ii1 = T
+    def Func4(self):
+        global VarS, VarT
+        VarS = self.TxtIn4.text
+        VarS = ToInt(VarS)
+        VarT = self.TxtIn5.text
+        VarT = ToInt(VarT)
+        if VarS == False or VarS <= 0 or VarT == False or VarT <= 0:
+            VarS = 0
+            VarT = 0
+            self.TxtIn4.text = str(VarS)
+            self.TxtIn5.text = str(VarT)
         else:
-            self.manager.current='mm1'
+            # Далее идет логика расчета по формуле Руфье
+            pass
 
-class NN(F):
-    pass
-
-class OO(F):
-    def __init__(self, **pp1):
-        super().__init__(**pp1)
-        global R, S, T
-        qq1 = D(orientation='vertical', padding=8, spacing=8)
-        rr1 = K(R, S, T, Z)
-        ss1 = B2(text='Результат:' + str(rr1), halign='right')
-        qq1.add_widget(ss1)
-        self.add_widget(qq1)
-
-
-class P(A):
+class ApPMain(ApP):
     def build(self):
-        tt1 = E()
-        tt1.add_widget(W(name='uu1'))
-        tt1.add_widget(PP(name='oo'))
-        tt1.add_widget(CC(name='bb1'))
-        tt1.add_widget(NN(name='vv1'))
-        tt1.add_widget(OO(name='mm1'))
-        return tt1
+        MG = SCrEenMG()
+        MG.add_widget(Scn1(name='Scn1'))
+        MG.add_widget(Scn2(name='Scn2'))
+        MG.add_widget(Scn3(name='Scn3'))
+        return MG
 
-uu2 = P()
-uu2.run()
+AppMain = ApPMain()
+AppMain.run()
